b628d626642fb01a30f7f4b4ba54c8e0d2bdb008
EmmanuelEmp <emmyochogwu@gmail.com>
2025-04-04 12:08:09 +0100

    added docker compose

--- post-service/src/utils/rabbitmq.ts
+++ post-service/src/utils/rabbitmq.ts
@@ -1,6 +1,9 @@
 import amqp from 'amqplib';
 import { logger } from './logger';
 
+const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
+const RABBITMQ_URL = process.env.RABBITMQ_URL || "amqp://localhost:5672";
+
 // Initialize connection and channel
 let connection = null;
 let channel: amqp.Channel | null = null;
@@ -11,7 +14,7 @@ const EXCHANGE_NAME = 'facebook-events';
 // Connect to RabbitMQ
 export async function connectRabbitMQ() {
     try {
-        connection = await amqp.connect(process.env.RABBITMQ_URL as string);
+        connection = await amqp.connect(RABBITMQ_URL as string);
         channel = await connection.createChannel();
         await channel.assertExchange(EXCHANGE_NAME, 'topic', { durable: false });
         logger.info('Connected to rabbit mq');
